aa0afb2dd487a86209ff0b37a3206ce30dc96d87
Airon Herbert <aironherbertms@gmail.com>
2022-04-22 21:25:06 -0300

    Initialize repository

---
+++ api/views.py
@@ -0,0 +1,98 @@
+from re import I
+from rest_framework import generics, permissions
+from yaml import serialize
+from .serializers import TodoSerializer, TodoToggleCompleteSerializer
+from todo.models import Todo
+from django.db import IntegrityError
+from django.contrib.auth.models import User
+from rest_framework.parsers import JSONParser
+from rest_framework.authtoken.models import Token
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate
+
+
+class TodoList(generics.ListAPIView):
+    # ListAPIView requires two mandatory attributes, serializer_class and
+    # queryset
+    # We specify TodoSerializer which we have earlier implemented
+
+    serializer_class = TodoSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user).order_by('-created')
+
+
+class TodoListCreate(generics.ListCreateAPIView):
+    # ListAPIView requires two mandatory attributes, serializer_class and
+    # queryset
+    # We specify TodoSerializer which we have earlier implemented
+
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user).order_by('-created')
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class TodoRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user)
+
+
+class TodoToggleComplete(generics.UpdateAPIView):
+    serializer_class = TodoToggleCompleteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user)
+
+    def perform_update(self, serializer):
+        serializer.instance.completed = not(serializer.instance.completed)
+        serializer.save()
+
+
+@csrf_exempt
+def signup(request):
+    if request.method == 'POST':
+        try:
+            data = JSONParser().parse(request)
+            user = User.objects.create_user(
+                username=data['username'], password=data['password'])
+            user.save()
+
+            token = Token.objects.create(user=user)
+            return JsonResponse({'token': str(token)}, status=201)
+        except IntegrityError:
+            return JsonResponse(
+                {'error': 'username taken. choose another username'}, status=400
+            )
+
+
+@csrf_exempt
+def login(request):
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        user = authenticate(
+            request, username=data['username'], password=data['password'])
+        if user is None:
+            return JsonResponse(
+                {'error': 'unable to login. check username and password'},
+                status=400
+            )
+        else:
+            try:
+                token = Token.objects.get(user=user)
+            except:
+                token = Token.objects.create(user=user)
+            return JsonResponse({'token': str(token)}, status=201)
